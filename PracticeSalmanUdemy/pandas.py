d87fe949d4ac7158c5e8f52663671279d340de10
Chandan Mukherjee <chandanmukherjee43@gmail.com>
2020-07-24 18:27:41 +0530

    'test'

--- PracticeSalmanUdemy/pandas.py
+++ PracticeSalmanUdemy/pandas.py
@@ -6,3 +6,5 @@ os.chdir(Relative_Project_Path.ROOT_DIR)
 # df = pd.read_csv('FL_insurance_sample.csv')
 df = pd.read_csv(r"C:\Users\Chandan Mukherjee\PycharmProjects\ProperProjectChandan\FL_insurance_sample.csv")
 print(df)
+
+
