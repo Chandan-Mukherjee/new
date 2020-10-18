b69c77d2b730f90d8fd4af9b395cdfffed16a022
ChandanMukherjee <mukherjeechandan369@gmail.com>
2020-10-18 18:21:22 +0530

    Config file data reader added

--- Naukri/naukri_page.py
+++ Naukri/naukri_page.py
@@ -1,27 +1,31 @@
 from selenium import webdriver
-import time
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
-import Relative_Project_Path
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from configparser import ConfigParser
+import os
+import Relative_Project_Path
 
-# This three lines are for opening chrome in incognito mode...
+# 1. This three lines are for opening chrome in incognito mode...
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("--incognito")
-
 driver = webdriver.Chrome(
     executable_path=Relative_Project_Path.driver_path + 'chromedriver.exe', options=chrome_options)
 print("\n--Opened Chrome in incognito mode.")
 
-sec = 3
+# 2. Config file reading
+print('\n---The current directory is: ' + str(os.getcwd()))
+os.chdir(Relative_Project_Path.config_init_file_path)
+config = ConfigParser()
+config.read('config.ini')
+# config['DEFAULT']['url']
+print('\n---Read the config file and data is collected from that.')
+os.chdir(Relative_Project_Path.naukri_path)
 
 try:
-    driver.get('https://www.naukri.com')
-    # time.sleep(3)
+    driver.get(config['naukri']['url'])
     driver.maximize_window()
-    # driver.quit()
     windows = driver.window_handles
     try:
         for i in windows:
@@ -31,26 +35,21 @@ try:
     except Exception as e:
         print(e)
     driver.switch_to.window(windows[0])
-    # driver.switch_to_default_content()
     driver.find_element_by_id('login_Layer').click()
-    # time.sleep(sec)
     element = WebDriverWait(driver, 15).until(
         EC.element_to_be_clickable((By.XPATH, "//input[@placeholder='Enter your active Email ID / Username']")))
     driver.find_element_by_xpath("//input[@placeholder='Enter your active Email ID / Username']").send_keys(
-        'chandanmukherjee43@gmail.com')
-    driver.find_element_by_xpath("//input[@placeholder='Enter your password']").send_keys('Sai@2020')
+        config['naukri']['email']
+    )
+    driver.find_element_by_xpath("//input[@placeholder='Enter your password']").send_keys(config['naukri']['password'])
     driver.find_element_by_xpath("//button[@type='submit']").click()
-    # driver.find_element_by_link_text('My Naukri').click()
-    # time.sleep(sec)
     element1 = WebDriverWait(driver, 15).until(
         EC.presence_of_element_located((By.XPATH, "//div[contains(text(),'My Naukri')]")))
     action = ActionChains(driver)
     action.move_to_element(
         driver.find_element_by_xpath("//div[contains(text(),'My Naukri')]")).perform()
-    # time.sleep(sec)
     element2 = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.LINK_TEXT, "Edit Profile")))
     action.move_to_element(driver.find_element_by_link_text('Edit Profile')).click().perform()
-    # time.sleep(sec)
     element3 = WebDriverWait(driver, 15).until(
         EC.element_to_be_clickable((By.XPATH, "//span[text()='Resume Headline']")))
     driver.find_element_by_xpath("//span[text()='Resume Headline']").click()
@@ -60,29 +59,27 @@ try:
     print(current_text)
     # driver.find_element_by_link_text('Save').click()
     driver.refresh()
-    # time.sleep(sec)
     driver.execute_script("window.scrollTo(0, 200);")
-    # time.sleep(sec)
     element4 = WebDriverWait(driver, 15).until(
         EC.element_to_be_clickable((By.XPATH, "//span[text()='Resume Headline']")))
     driver.find_element_by_xpath("//span[text()='Resume Headline']").click()
     driver.find_element_by_xpath("//div[@class='resumeHeadline']/div/div/div/span[2]").click()
-    if current_text == 'QA Engineer with 4.6 years of Experience including Manual and Automation Testing. Currently working as a Test Engineer at Tech Mahindra Ltd. Area of Expertise is Selenium with Python, CRM Testing and Cloud Platform.':
+    if current_text == config['naukri']['CurrentText']:
         driver.execute_script("window.scrollTo(0, 200);")
         print("\n----Resume Headline changed to: ")
         print(
-            "QA Engineer with 4.6 years of Experience including Manual and Automation Testing. Currently working as a Test Engineer at Tech Mahindra Ltd. Area of Expertise is Selenium with Python, CRM Testing and Cloud Platform")
+            config['naukri']['OtherText'])
         driver.find_element_by_xpath('//*[@id="resumeHeadlineTxt"]').clear()
         driver.find_element_by_xpath('//*[@id="resumeHeadlineTxt"]').send_keys(
-            'QA Engineer with 4.6 years of Experience including Manual and Automation Testing. Currently working as a Test Engineer at Tech Mahindra Ltd. Area of Expertise is Selenium with Python, CRM Testing and Cloud Platform')
+            config['naukri']['OtherText'])
     else:
         driver.execute_script("window.scrollTo(0, 200);")
         driver.find_element_by_xpath('//*[@id="resumeHeadlineTxt"]').clear()
         print("\n----Resume Headline changed to: ")
         print(
-            'QA Engineer with 4.6 years of Experience including Manual and Automation Testing. Currently working as a Test Engineer at Tech Mahindra Ltd. Area of Expertise is Selenium with Python, CRM Testing and Cloud Platform.')
+            config['naukri']['CurrentText'])
         driver.find_element_by_xpath('//*[@id="resumeHeadlineTxt"]').send_keys(
-            'QA Engineer with 4.6 years of Experience including Manual and Automation Testing. Currently working as a Test Engineer at Tech Mahindra Ltd. Area of Expertise is Selenium with Python, CRM Testing and Cloud Platform.')
+            config['naukri']['CurrentText'])
     driver.find_element_by_xpath(
         "//button[text()='Save' and @class='waves-effect waves-light btn-large blue-btn' ]").click()
     # driver.find_element_by_xpath('//*[@id="resumeHeadlineTxt"]').send_keys(Keys.RETURN)
@@ -92,11 +89,9 @@ try:
     element5 = WebDriverWait(driver, 15).until(
         EC.element_to_be_clickable((By.XPATH, '//img[@src="https://static.naukimg.com/s/4/100/i/naukri_Logo.png"]')))
     driver.find_element_by_xpath('//img[@src="https://static.naukimg.com/s/4/100/i/naukri_Logo.png"]').click()
-    # time.sleep(sec)
     print('\n----Clicked on main naukri logo-----')
     # action.move_to_element(
     #     driver.find_element_by_xpath('//a[@href="https://my.naukri.com/HomePage/view"]')).perform()
-    # time.sleep(sec)
     # driver.find_element_by_xpath("//a[contains(text(),'Logout')]").click()
     print("\n----Test Successful, Updated your naukri profile----")
     print("\n---Smile :):):) ---")
--- Relative_Project_Path.py
+++ Relative_Project_Path.py
@@ -3,3 +3,5 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 screenshotpath = os.path.join(os.path.sep, ROOT_DIR, 'Screenshots' + os.sep)
 driver_path = os.path.join(os.path.sep, ROOT_DIR, 'Browsers' + os.sep)
+config_init_file_path = os.path.join(os.path.sep, ROOT_DIR, 'Tests' + os.sep)
+naukri_path = os.path.join(os.path.sep, ROOT_DIR, 'Naukri' + os.sep)
